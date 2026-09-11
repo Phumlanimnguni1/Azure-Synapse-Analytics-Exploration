d5bdb135263ea8c95ea3cdcdafcac52d34b49ddc
phumlanimnguni1 <prosperproper700@gmail.com>
2026-09-11 04:50:01 +0200

    Execute PowerShell setup scripts to deploy the foundational Synapse architecture, including the dedicated SQL pool, Apache Spark pool, Data Explorer pool, and attached Azure Data Lake Storage Gen2

---
+++ src/infrastructure/synapeStudio.ps1
@@ -0,0 +1,8 @@
+rm -r dp-000 -f
+git clone https://github.com/MicrosoftLearning/mslearn-synapse dp-000
+
+#  After the repo has been cloned, 
+# enter the following commands to change to the folder for this lab and run the setup.ps1 script it contains:
+cd dp-000/Allfiles/Labs/01
+ ./setup.ps1
+
